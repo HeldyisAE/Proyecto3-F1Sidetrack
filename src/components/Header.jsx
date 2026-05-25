02e96d8d5a014a798f949f7d921b5a9a94a7b75d
HeldyisAE <heaguero@estudiantec.cr>
2026-05-24 21:27:43 -0600

    Organiza la estructura visual del display en pantalla

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import '../styles/Header.css'
+
+function Header() {
+
+    return(
+        <div className="header">
+            <div className="left">
+                <h1>Left</h1>
+            </div>
+            <div className="center">
+                <h1>Center</h1>
+            </div>
+            <div className="right">
+                <h1>Right</h1>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import '../styles/Home.css'
 
+import Header from '../components/Header';
+
 function Home() {
 
     return (
         <div className="home">
             <div className="top">
-                <h1>Header</h1>
+                <Header />
             </div>
             <div className="mid">
-                <h1>Content</h1>
+                <div className='content-container'>
+                    <h1>Content</h1>  
+                </div>
             </div>
             <div className="bottom">
                 <h1>Footer</h1>
