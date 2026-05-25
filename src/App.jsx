4f593b2edca3b0ac8064317347f05478b5812f75
HeldyisAE <heaguero@estudiantec.cr>
2026-05-24 19:19:32 -0600

    Implementa la página home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div></div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </>
+  );
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import '../styles/Home.css'
+
+function Home() {
+
+    return (
+        <div className="home">
+            <div className="top">
+                <h1>Header</h1>
+            </div>
+            <div className="mid">
+                <h1>Content</h1>
+            </div>
+            <div className="bottom">
+                <h1>Footer</h1>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
