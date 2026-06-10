cc4d7d9d037c7478fc7b1e06d092029478f5ee42
HeldyisAE <heaguero@estudiantec.cr>
2026-06-10 01:10:17 -0600

    Implementa la página de equipo

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 import Driver from "./pages/Driver";
+import Team from "./pages/Team";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/shop" element={<Shop />}/>
         <Route path="/cart" element={<Cart />}/>
         <Route path="/driver/:driverNumber" element={<Driver />}/>
+        <Route path="/team/:teamId" element={<Team />} />
       </Routes>
     </ThemeProvider>
   );
