5452cd372f4952a1d5c3b3d273a97a762f6756ce
YeremiCalvoPorras <calvoporrasyeremi@gmail.com>
2026-06-11 06:50:31 -0600

    arreglo conflicto
    se arreglo el conflicto y se conectaron las partes de calendario y resultados

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
+// src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import Shop from "./pages/Shop";
-import Cart from "./pages/Cart";
-import Driver from "./pages/Driver";
-import Team from "./pages/Team";
+import Login      from "./pages/Login";
+import Home       from "./pages/Home";
+import Shop       from "./pages/Shop";
+import Cart       from "./pages/Cart";
+import Driver     from "./pages/Driver";
+import Team       from "./pages/Team";      
 import ShopSearch from "./pages/ShopSearch";
+import Schedule   from "./pages/Schedule";
+import Results    from "./pages/Results";
 
 function App() {
   return (
-      <ThemeProvider>
-          <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/shop" element={<Shop />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/driver/:driverNumber" element={<Driver />} />
-              <Route path="/team/:teamId" element={<Team />} />
-              <Route path="/shop/search" element={<ShopSearch />} />
-          </Routes>
-      </ThemeProvider>
+    <ThemeProvider>
+      <Routes>
+        <Route path="/"                      element={<Login />} />
+        <Route path="/home"                  element={<Home />} />
+        <Route path="/shop"                  element={<Shop />} />
+        <Route path="/cart"                  element={<Cart />} />
+        <Route path="/driver/:driverNumber"  element={<Driver />} />
+        <Route path="/team/:teamId"          element={<Team />} />
+        <Route path="/shop/search"           element={<ShopSearch />} />
+        <Route path="/schedule"              element={<Schedule />} />
+        <Route path="/results"               element={<Results />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
--- src/components/RedirectButtons.jsx
+++ src/components/RedirectButtons.jsx
@@ -1,18 +1,19 @@
 import "../styles/RedirectButtons.css";
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 import ComingSoonModal from "./ComingSoonModal";
 
 function RedirectButtons({ activeMenu, setActiveMenu }) {
     const { t } = useTranslation();
-
+    const navigate = useNavigate();
     const [showComingSoon, setShowComingSoon] = useState(false);
 
     return (
         <div className="redirect-container">
             <button
                 className="redirect-button"
+                onClick={() => navigate('/schedule')}
                 onMouseEnter={() => setActiveMenu(null)}
             >
                 {t("navigation.schedule")}
@@ -20,17 +21,17 @@ function RedirectButtons({ activeMenu, setActiveMenu }) {
 
             <button
                 className="redirect-button"
+                onClick={() => navigate('/results')}
                 onMouseEnter={() => setActiveMenu(null)}
             >
-                {t("navigation.results")}{" "}
+                {t("navigation.results")}
             </button>
 
             <button
                 className="redirect-button"
                 onMouseEnter={() => setActiveMenu("drivers")}
             >
-                {" "}
-                {t("navigation.pilots")}{" "}
+                {t("navigation.pilots")}
             </button>
 
             <button
