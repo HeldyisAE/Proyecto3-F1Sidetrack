18b4180b67587167cff1823ea7f2467d524e8654
HeldyisAE <heaguero@estudiantec.cr>
2026-06-10 23:05:10 -0600

    Añade un botón para volver a la tienda desde la página de results

--- src/pages/ShopSearch.jsx
+++ src/pages/ShopSearch.jsx
@@ -1,8 +1,13 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ShopSearchLayout from "../layouts/ShopSearchLayout";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 function ShopSearch() {
+    
+    const navigate = useNavigate();
+
     return (
         <div className="home">
             <div className="top">
@@ -11,7 +16,16 @@ function ShopSearch() {
 
             <div className="mid">
                 <div className="content-container">
-                    <ShopSearchLayout />
+                    <div className="shop-search-content">
+                        <button
+                            className="shop-search-back-button"
+                            onClick={() => navigate("/shop")}
+                        >
+                            <IoMdArrowRoundBack />
+                        </button>
+
+                        <ShopSearchLayout />
+                    </div>
                 </div>
             </div>
 
